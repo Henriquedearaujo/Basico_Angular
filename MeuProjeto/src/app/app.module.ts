51d232b378ce0ab8ec0c9e6077e3e70707820c7f
Henrique De Araujo <ricke_cris@hotmail.com>
2022-12-27 18:39:09 -0300

    Definindo as rotas sobre e contato

--- MeuProjeto/src/app/app.module.ts
+++ MeuProjeto/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouterConfing } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfing, { useHash: false})]
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
